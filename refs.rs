419182da0ec978946645f71428e78f631676df9b
C Ding <cding@cs.rochester.edu>
2023-05-18 14:32:36 -0400

    Update refs.rs

--- refs.rs
+++ refs.rs
@@ -1,9 +1,11 @@
+use crate::loops::{IterVec, AccVec};
+
 /// An array reference
 /// Each array reference resides in a loop nest.
-pub struct ArrayRef {
+pub struct ArrRef {
     name: String,
     /// Subscript expressions: one function for each data dimension.  
     /// Each function takes the indices of its loop nest and returns indices 
     /// of the array access.
-    subexprs: Vec<fn(Vec<i32>) -> Vec<i32>>
+    subexprs: Vec<fn(IterVec) -> ArrAcc>
 }
