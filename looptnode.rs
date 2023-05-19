659c446cbdab4a62e086645bbc9e3099d82a509f
C Ding <cding@cs.rochester.edu>
2023-05-19 08:54:39 -0400

    Update looptnode.rs

--- looptnode.rs
+++ looptnode.rs
@@ -4,7 +4,15 @@ use std::rc::{Rc, Weak};
 /// Each loop and statement is a node in a loop tree.
 pub enum LoopTNode {
     /// A single loop
-    Loop {
+    Loop (SingleLoop),
+    /// A statement is a sequence of array references
+    Ref (RefStmt)
+}
+
+pub type LTNodesRef = RefCell<Vec<Rc<LoopTNode>>>;
+pub type LTNodeWeakRef = RefCell<Weak<LoopTNode>>;
+
+pub struct SingleLoop {
         iv: String,
         lb: LoopBound,
         ub: LoopBound,
@@ -13,16 +21,13 @@ pub enum LoopTNode {
         step: fn(i32)->i32,
         body: LTNodesRef,
         parent: LTNodeWeakRef
-    },
-    /// A statement is a sequence of array references
-    RefStmt {
-        refs: Vec<ArrRef>,
-        parent: LTNodeWeakRef
-    }
 }
 
-pub type LTNodesRef = RefCell<Vec<Rc<LoopTNode>>>;
-pub type LTNodeWeakRef = RefCell<Weak<LoopTNode>>;
+pub struct RefStmt {
+        refs: Vec<ArrRef>,
+        parent: LTNodeWeakRef
+}
+
 
 pub enum LoopBound {
     Fixed(i32),
@@ -40,3 +45,30 @@ pub struct ArrRef {
 pub type IterVec = Vec<i32>;
 pub type ArrAcc = Vec<i32>;
 
+impl LoopTNode {
+    fn sanity(&self) -> i32 {
+        match self {
+            LoopTNode::Loop(aloop) => aloop.body.borrow().iter()
+                .fold(0, |acc, x| acc + x.sanity()),
+            LoopTNode::Ref(_) => 1,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{LoopTNode::*, RefStmt};
+    use std::rc::Weak;
+    
+    #[test]
+    fn sanity() {
+        let aref = RefStmt{refs: Vec::new(), parent: Weak::new().into()};
+        let node = Ref(aref);
+        assert_eq!(node.sanity(), 1);
+    }
+    
+    #[test]
+    fn matmul() {
+        
+    }
+}
