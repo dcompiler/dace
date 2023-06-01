de6389d3f61b62fa6fb876e16b9325f52c30e376
woody <woodyy.wu@outlook.com>
2023-06-01 09:35:08 -0400

    Removed RefStmt Struct. Added test and step.

---
+++ src/trace.rs
@@ -0,0 +1,13 @@
+// fro each array ref, calute adreaa
+// for all the create, we can just add the call.
+
+
+// In the unite test, add loop for one reference and check correctness.
+
+//  How do we do sampling once we have trace?  ----> sampler.rs.
+
+
+
+// For each reference, we get a loop node.
+//
+// for each sample, we get to the refer for that loop interation,
