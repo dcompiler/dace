63595206397d274711b0cdf5e962313c5b70d4c4
C Ding <cding@cs.rochester.edu>
2023-06-01 22:35:42 -0400

    trace

--- src/looptnode.rs
+++ src/looptnode.rs
@@ -16,7 +16,7 @@ pub enum Stmt {
     Loop(LoopStmt),
     /// A statement is a sequence of array references
     // Ref(RefStmt),
-    Ref(ArrRef),
+    Ref(AryRef),
 }
 
 pub struct LoopStmt {
@@ -25,7 +25,7 @@ pub struct LoopStmt {
     pub ub: LoopBound,
     // The next two need the FnOnce trait, which we'll add later
     // Now we assume test is iv < ub
-    pub test: fn(i32, i32) -> bool,
+    pub test: fn(i32,i32) -> bool,
     // Now we assume step is iv = iv + 1
     pub step: fn(i32) -> i32,
     pub body: LTNodesRef,
@@ -34,38 +34,46 @@ pub struct LoopStmt {
 
 pub enum LoopBound {
     Fixed(i32),
-    Dynamic(fn(IterVec) -> i32),
+    Dynamic(fn(&IterVec) -> i32),
 }
 
 // pub struct RefStmt {
-//     pub refs: Vec<ArrRef>,
+//     pub refs: Vec<AryRef>,
 // }
 
 /// Array reference.
 #[derive(Clone)]
-pub struct ArrRef {
-    name: String,
+pub struct AryRef {
+    pub name: String,
+    /// array dimensions, e.g. [5,5]
+    pub dim: Vec<usize>,
     /// Subscript expressions: one function for each data dimension.  
     /// Each function takes the indices of its loop nest and returns indices of the array access.
-    pub sub: fn(IterVec) -> ArrAcc,
+    pub sub: fn(&IterVec) -> ArrAcc,
 }
 
 /// Type alias for the iteration vector.
-pub type IterVec = Vec<i32>;
+pub type IterVec = Vec<usize>;
 /// Type alias for the array access indices.
-pub type ArrAcc = Vec<i32>;
+pub type ArrAcc = Vec<usize>;
 
 impl LoopTNode {
     /// Create a new LoopTNode with a given statement.
-    fn new_ref(a_stmt: Stmt) -> Rc<LoopTNode> {
+    pub fn new_node(a_stmt: Stmt) -> Rc<LoopTNode> {
         return Rc::new(LoopTNode {
             stmt: a_stmt,
             parent: RefCell::new(Weak::new()),
         });
     }
 
+    pub fn new_ref(ary_nm: &str, ary_dim: Vec<usize>,
+		   ary_sub: fn(&Vec<usize>)->Vec<usize>) -> Rc<LoopTNode> {
+	let ref_stmt = AryRef { name: ary_nm.to_string(), dim: ary_dim, sub: ary_sub };
+	LoopTNode::new_node(Stmt::Ref(ref_stmt))
+    }
+
     /// Create a new LoopTNode representing a simple loop with a fixed range.
-    fn new_simple_loop_ref(ivar: &str, low: i32, high: i32) -> Rc<LoopTNode> {
+    pub fn new_single_loop(ivar: &str, low: i32, high: i32) -> Rc<LoopTNode> {
         let loop_stmt = LoopStmt {
             iv: ivar.to_string(),
             lb: LoopBound::Fixed(low),
@@ -75,11 +83,11 @@ impl LoopTNode {
             step: |i| i + 1,
             body: RefCell::new(vec![]),
         };
-        LoopTNode::new_ref(Stmt::Loop(loop_stmt))
+        LoopTNode::new_node(Stmt::Loop(loop_stmt))
     }
 
     /// Extend the body of a loop node with another node.
-    fn extend_loop_body(loop_ref: &Rc<LoopTNode>, stmt_ref: &Rc<LoopTNode>) {
+    pub fn extend_loop_body(loop_ref: &Rc<LoopTNode>, stmt_ref: &Rc<LoopTNode>) {
         if let Stmt::Loop(ref lp) = loop_ref.stmt {
             // officiating the parent-child relationship
             *stmt_ref.parent.borrow_mut() = Rc::downgrade(loop_ref);
@@ -122,34 +130,35 @@ mod tests {
 
     #[test]
     fn acc_ref() {
-        let ar = ArrRef { name: "X".to_string(), sub: |iv| vec![iv[0] + 1] };
-        assert_eq!((ar.sub)(vec![1]), [2]);
+        let ar = AryRef { name: "X".to_string(), dim: vec![10], sub: |iv| vec![iv[0] + 1] };
+        assert_eq!((ar.sub)(&vec![1]), [2]);
     }
 
     #[test]
     fn matmul() {
-        let n = 100;
+        let n: usize = 100;  // array dim
+	let ubound = n as i32;  // loop bound
         // creating C[i,j] += A[i,k] * B[k,j]
-        let c = ArrRef { name: "C".to_string(), sub: |ijk| vec![ijk[0], ijk[1]] };
-        let a = ArrRef { name: "A".to_string(), sub: |ijk| vec![ijk[0], ijk[2]] };
-        let b = ArrRef { name: "B".to_string(), sub: |ijk| vec![ijk[2], ijk[1]] };
+        let c = AryRef { name: "C".to_string(), dim: vec![n,n], sub: |ijk| vec![ijk[0], ijk[1]] };
+        let a = AryRef { name: "A".to_string(), dim: vec![n,n], sub: |ijk| vec![ijk[0], ijk[2]] };
+        let b = AryRef { name: "B".to_string(), dim: vec![n,n], sub: |ijk| vec![ijk[2], ijk[1]] };
         // let s = RefStmt { refs: vec![c.clone(), c, a, b] };
         // let s = vec![c.clone(), c, a, b];
-        // let s_ref = LoopTNode::new_ref(Stmt::Ref(s));
-        let s_ref_c = LoopTNode::new_ref(Stmt::Ref(c));
-        let s_ref_a = LoopTNode::new_ref(Stmt::Ref(a));
-        let s_ref_b = LoopTNode::new_ref(Stmt::Ref(b));
+        // let s_ref = LoopTNode::new_node(Stmt::Ref(s));
+        let s_ref_c = LoopTNode::new_node(Stmt::Ref(c));
+        let s_ref_a = LoopTNode::new_node(Stmt::Ref(a));
+        let s_ref_b = LoopTNode::new_node(Stmt::Ref(b));
 
         // creating loop k = 0, n { s_ref }
-        let k_loop_ref = LoopTNode::new_simple_loop_ref("k", 0, n);
+        let k_loop_ref = LoopTNode::new_single_loop("k", 0, ubound);
         LoopTNode::extend_loop_body(&k_loop_ref, &s_ref_c);
         LoopTNode::extend_loop_body(&k_loop_ref, &s_ref_a);
         LoopTNode::extend_loop_body(&k_loop_ref, &s_ref_b);
         // creating loop j = 0, n
-        let j_loop_ref = LoopTNode::new_simple_loop_ref("j", 0, n);
+        let j_loop_ref = LoopTNode::new_single_loop("j", 0, ubound);
         LoopTNode::extend_loop_body(&j_loop_ref, &k_loop_ref);
         // creating loop i = 0, n
-        let i_loop_ref = LoopTNode::new_simple_loop_ref("i", 0, n);
+        let i_loop_ref = LoopTNode::new_single_loop("i", 0, ubound);
         LoopTNode::extend_loop_body(&i_loop_ref, &j_loop_ref);
 
         assert_eq!(i_loop_ref.node_count(), 6);
@@ -160,7 +169,7 @@ mod tests {
         // 			    lb: LoopBound::Fixed(0), ub: LoopBound::Fixed(n),
         // 			    // test: |k| k<n , step: |k| k+1,
         // 			    body: RefCell::new(vec![]) };
-        // let k_loop_ref = LoopTNode::new_ref( Stmt::Loop(k_loop_stmt) );
+        // let k_loop_ref = LoopTNode::new_node( Stmt::Loop(k_loop_stmt) );
         // let k_loop_ref = Rc::new(
         //     LoopTNode{ stmt: Stmt::Loop(k_loop_stmt),
         // 	       parent: RefCell::new(Weak::new())
@@ -170,9 +179,9 @@ mod tests {
         //     *(lp.body.borrow())[0].parent.borrow_mut() = Rc::downgrade(&k_loop_ref);
         // }
         // let j_loop_stmt = LoopStmt{ iv: "j".to_string(),
-        // 			    lb: LoopBound::Fixed(0), ub: LoopBound::Fixed(n),
+        // 			    lb: LoopBound::Fixed(0), ub: LoopBound::Fixed(ubound),
         // 			    body: RefCell::new(vec![]) };
-        // let j_loop_ref = LoopTNode::new_ref( Stmt::Loop(j_loop_stmt) );
+        // let j_loop_ref = LoopTNode::new_node( Stmt::Loop(j_loop_stmt) );
         // let j_loop_ref = Rc::new(
         //     LoopTNode{ stmt: Stmt::Loop(j_loop_stmt),
         // 	       parent: RefCell::new(Weak::new())
@@ -181,9 +190,9 @@ mod tests {
         //     *(lp.body.borrow())[0].parent.borrow_mut() = Rc::downgrade(&j_loop_ref);
         // }
         // let i_loop_stmt = LoopStmt{ iv: "i".to_string(),
-        // 			    lb: LoopBound::Fixed(0), ub: LoopBound::Fixed(n),
+        // 			    lb: LoopBound::Fixed(0), ub: LoopBound::Fixed(ubound),
         // 			    body: RefCell::new(vec![]) };
-        // let i_loop_ref = LoopTNode::new_ref( Stmt::Loop(i_loop_stmt) );
+        // let i_loop_ref = LoopTNode::new_node( Stmt::Loop(i_loop_stmt) );
         // let i_loop_ref = Rc::new(
         //     LoopTNode{ stmt: Stmt::Loop(i_loop_stmt),
         // 	       parent: RefCell::new(Weak::new())
@@ -195,27 +204,28 @@ mod tests {
 
     #[test]
     fn mat_transpose1() {
-        let n = 1024;
+        let n: usize = 1024;
+	let ubound = n as i32;
         // for (int c0 = 0; c0 < n; c0 += 1)
         // for (int c1 = 0; c1 < n; c1 += 1)
         //   x1[c0] = (x1[c0] + (A[c0][c1] * y_1[c1]));
-        let x1 = ArrRef { name: "x1".to_string(), sub: |i| vec![i[0]] };
-        let a = ArrRef { name: "A".to_string(), sub: |ij| vec![ij[0], ij[1]] };
-        let y1 = ArrRef { name: "y1".to_string(), sub: |j| vec![j[0], j[1]] };
+        let x1 = AryRef { name: "x1".to_string(), dim: vec![n], sub: |i| vec![i[0]] };
+        let a = AryRef { name: "A".to_string(), dim: vec![n,n], sub: |ij| vec![ij[0], ij[1]] };
+        let y1 = AryRef { name: "y1".to_string(), dim: vec![n], sub: |j| vec![j[0], j[1]] };
         // let s = RefStmt{ refs: vec![x1.clone(), x1.clone(), a.clone(), y1.clone()] };
         // let s = vec![x1.clone(), x1.clone(), a.clone(), y1.clone()];
 
 
-        let s_ref_x1 = LoopTNode::new_ref(Stmt::Ref(x1));
-        let s_ref_a = LoopTNode::new_ref(Stmt::Ref(a));
-        let s_ref_y1 = LoopTNode::new_ref(Stmt::Ref(y1));
+        let s_ref_x1 = LoopTNode::new_node(Stmt::Ref(x1));
+        let s_ref_a = LoopTNode::new_node(Stmt::Ref(a));
+        let s_ref_y1 = LoopTNode::new_node(Stmt::Ref(y1));
 
-        let j_loop_ref = LoopTNode::new_simple_loop_ref("j", 0, n);
+        let j_loop_ref = LoopTNode::new_single_loop("j", 0, ubound);
         LoopTNode::extend_loop_body(&j_loop_ref, &s_ref_x1);
         LoopTNode::extend_loop_body(&j_loop_ref, &s_ref_a);
         LoopTNode::extend_loop_body(&j_loop_ref, &s_ref_y1);
 
-        let i_loop_ref = LoopTNode::new_simple_loop_ref("i", 0, n);
+        let i_loop_ref = LoopTNode::new_single_loop("i", 0, ubound);
         LoopTNode::extend_loop_body(&i_loop_ref, &j_loop_ref);
 
         assert_eq!(i_loop_ref.node_count(), 5);
@@ -223,28 +233,29 @@ mod tests {
 
     #[test]
     fn mat_transpose2() {
-        let n = 1024;
+        let n: usize = 1024;
+	let ubound = n as i32;
         //     for (int c0 = 0; c0 < n; c0 += 1)
         //     for (int c1 = 0; c1 < n; c1 += 1)
         //     x2[c0] = (x2[c0] + (A[c1][c0] * y_2[c1]));
-        let x2 = ArrRef { name: "x2".to_string(), sub: |i| vec![i[0]] };
-        let a = ArrRef { name: "A".to_string(), sub: |ij| vec![ij[0], ij[1]] };
-        let y2 = ArrRef { name: "y2".to_string(), sub: |j| vec![j[0], j[1]] };
+        let x2 = AryRef { name: "x2".to_string(), dim: vec![n], sub: |i| vec![i[0]] };
+        let a = AryRef { name: "A".to_string(), dim: vec![n,n], sub: |ij| vec![ij[0], ij[1]] };
+        let y2 = AryRef { name: "y2".to_string(), dim: vec![n], sub: |j| vec![j[0], j[1]] };
         // let s = RefStmt{ refs: vec![x2.clone(), x2.clone(), a.clone(), y2.clone()] };
         // let s = vec![x2.clone(), x2.clone(), a.clone(), y2.clone()];
-        // let s_ref = LoopTNode::new_ref(Stmt::Ref(s));
+        // let s_ref = LoopTNode::new_node(Stmt::Ref(s));
 
-        let s_ref_x1 = LoopTNode::new_ref(Stmt::Ref(x2));
-        let s_ref_a = LoopTNode::new_ref(Stmt::Ref(a));
-        let s_ref_y1 = LoopTNode::new_ref(Stmt::Ref(y2));
+        let s_ref_x1 = LoopTNode::new_node(Stmt::Ref(x2));
+        let s_ref_a = LoopTNode::new_node(Stmt::Ref(a));
+        let s_ref_y1 = LoopTNode::new_node(Stmt::Ref(y2));
 
 
-        let j_loop_ref = LoopTNode::new_simple_loop_ref("j", 0, n);
+        let j_loop_ref = LoopTNode::new_single_loop("j", 0, ubound);
         LoopTNode::extend_loop_body(&j_loop_ref, &s_ref_x1);
         LoopTNode::extend_loop_body(&j_loop_ref, &s_ref_a);
         LoopTNode::extend_loop_body(&j_loop_ref, &s_ref_y1);
 
-        let i_loop_ref = LoopTNode::new_simple_loop_ref("i", 0, n);
+        let i_loop_ref = LoopTNode::new_single_loop("i", 0, ubound);
         LoopTNode::extend_loop_body(&i_loop_ref, &j_loop_ref);
 
         assert_eq!(i_loop_ref.node_count(), 5);
--- src/trace.rs
+++ src/trace.rs
@@ -1,3 +1,96 @@
+use crate::looptnode::*;
+use lru_stack::LRUStack;
+use hist::Hist;
+use ascii_converter::string_to_decimals;
+use std::rc::Rc;
+
+// Handling a single array ONLY.
+fn access2addr(ary_ref: &AryRef, ivec: &Vec<usize>) -> usize {
+    // let ary_initial: usize = (string_to_decimals(&ary_ref.name).unwrap())[0] as usize;
+    // let base = ary_initial * MAX_ARRAY;
+    
+    let ary_index = (ary_ref.sub)(ivec);
+    if ary_index.len() != ary_ref.dim.len() { panic!("array index and dimension do not match"); }
+    
+    let offset = ary_index.iter().zip(ary_ref.dim.iter())
+	.fold(0, |acc, (&i, &d)| acc*d + i);
+    
+    return offset;
+}
+
+fn trace_rec(code: &Rc<LoopTNode>, ivec: &Vec<usize>, sim: &mut LRUStack<usize>, hist: &mut Hist) {
+    match &code.stmt {
+	Stmt::Ref(ary_ref) => {let addr = access2addr(&ary_ref, &ivec);
+			       let rd = sim.rec_access(addr);
+			       hist.add_dist(rd);},
+	Stmt::Loop(aloop) => {
+	    let mut myvec = ivec.clone();
+	    myvec.push(0);
+	    if let LoopBound::Fixed(lb) = aloop.lb {
+		if let LoopBound::Fixed(ub) = aloop.ub {
+		    ((lb as usize)..(ub as usize)).into_iter().for_each(
+			|i| {
+			*myvec.last_mut().unwrap() = i;
+			aloop.body.borrow().iter().for_each(
+			    |stmt|
+			    trace_rec(stmt, &myvec, sim, hist) )})
+		}
+		else {panic!("dynamic loop upper bound is not supported")}
+	    }
+	    else {panic!("dynamic loop lower bound is not supported")}
+	}
+    }
+}
+
+pub fn trace(code: &Rc<LoopTNode>) -> Hist {
+    let mut sim = LRUStack::<usize>::new();
+    let mut hist = Hist::new();
+    trace_rec(code, &Vec::<usize>::new(), &mut sim, &mut hist);
+    hist
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_access2addr() {
+	let aij_node = LoopTNode::new_ref("x", vec![10,10], |ij| vec![ij[0], ij[1]]);
+	if let Stmt::Ref(aij) = &aij_node.stmt {
+	    assert_eq!(access2addr(&aij, &vec![0,0]), 0);
+	    assert_eq!(access2addr(&aij, &vec![9,9]), 99);
+	}
+    }
+
+    #[test]
+    fn loop_a_i() {
+
+        // i = 0, n { a[i] }
+        let aref = LoopTNode::new_ref("A", vec![10], |i| vec![i[0]]);
+        let aloop = LoopTNode::new_single_loop("i", 0, 10);
+        LoopTNode::extend_loop_body(&aloop, &aref);
+
+	let hist = trace(&aloop);
+	assert_eq!(hist.to_vec()[0], (None, 10));
+	println!("{}", hist);	
+    }
+
+    #[test]
+    fn loop_a_0() {
+
+        // i = 0, n { a[0] }
+        let aref = LoopTNode::new_ref("A", vec![1], |_| vec![0]);
+        let aloop = LoopTNode::new_single_loop("i", 0, 10);
+        LoopTNode::extend_loop_body(&aloop, &aref);
+
+	let hist = trace(&aloop);
+	assert_eq!(hist.to_vec()[0], (Some(1), 9));
+	assert_eq!(hist.to_vec()[1], (None, 1));
+	println!("{}", hist);	
+    }
+}
+
+
 // fro each array ref, calute adreaa
 // for all the create, we can just add the call.
 
