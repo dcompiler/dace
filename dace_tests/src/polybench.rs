2eb24739e1f9e463ac5d019b85088f6efcc5e318
Yekai Pan <51003352+QClr@users.noreply.github.com>
2023-06-15 14:57:44 -0400

    Added 2mm test

--- dace_tests/src/polybench.rs
+++ dace_tests/src/polybench.rs
@@ -204,6 +204,53 @@ pub fn syr2d(n: usize, m: usize) -> Rc<Node> {
     i_loop_ref
 }
 
+fn _2mm(NI: usize, NJ: usize, NK: usize, NL: usize) -> Rc<Node> {
+    let s_ref_tmp = Node::new_ref("tmp", vec![NI, NJ], |ijk| {
+        vec![ijk[0] as usize, ijk[1] as usize]
+    });
+    let s_ref_a = Node::new_ref("a", vec![NI, NK], |ijk| {
+        vec![ijk[0] as usize, ijk[2] as usize]
+    });
+    let s_ref_b = Node::new_ref("b", vec![NK, NJ], |ijk| {
+        vec![ijk[2] as usize, ijk[1] as usize]
+    });
+    let s_ref_c = Node::new_ref("c", vec![NL, NJ], |ijk| {
+        vec![ijk[3] as usize, ijk[1] as usize]
+    });
+    let s_ref_d = Node::new_ref("d", vec![NI, NL], |ijk| {
+        vec![ijk[0] as usize, ijk[3] as usize]
+    });
+
+    let knk_loop_ref = Node::new_single_loop("k", 0, NK as i32);
+    Node::extend_loop_body(&knk_loop_ref, &s_ref_a);
+    Node::extend_loop_body(&knk_loop_ref, &s_ref_b);
+    Node::extend_loop_body(&knk_loop_ref, &s_ref_tmp);
+    
+    let jnj_loop_ref = Node::new_single_loop("j", 0, NJ as i32);
+    Node::extend_loop_body(&knk_loop_ref, &s_ref_tmp);
+    Node::extend_loop_body(&knk_loop_ref, &knk_loop_ref);
+    
+    let ini_loop_ref1 = Node::new_single_loop("i", 0, NI as i32);
+    Node::extend_loop_body(&ini_loop_ref1, &jnj_loop_ref);
+
+    let knj_loop_ref = Node::new_single_loop("k", 0, NJ as i32);
+    Node::extend_loop_body(&knj_loop_ref, &s_ref_tmp);
+    Node::extend_loop_body(&knj_loop_ref, &s_ref_c);
+    Node::extend_loop_body(&knj_loop_ref, &s_ref_d);
+    
+    let jnl_loop_ref = Node::new_single_loop("j", 0, NL as i32);
+    Node::extend_loop_body(&jnj_loop_ref, &s_ref_d);
+    Node::extend_loop_body(&jnj_loop_ref, &knj_loop_ref);
+
+    let ini_loop_ref2 = Node::new_single_loop("i", 0, NI as i32);
+    Node::extend_loop_body(&ini_loop_ref2, &jnl_loop_ref);
+
+
+    Node::new_node(Stmt::Block(vec![ini_loop_ref1, ini_loop_ref2]))
+
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -233,5 +280,10 @@ mod tests {
     #[test]
     fn test_syr2d() {
         assert_eq!(syr2d(1024, 1024).node_count(), 12);
+        
+    }
+    #[test]
+    fn _2mm_test() {
+        assert_eq!(_2mm(1024, 1024, 1024, 1024).node_count(), 10);
     }
 }
