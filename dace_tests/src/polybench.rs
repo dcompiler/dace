1edf3d1d04f37e98c8dbc52766bb2fa6f6b410f8
Yekai Pan <51003352+QClr@users.noreply.github.com>
2023-06-15 15:07:46 -0400

    Fixed 2mm fmt

--- dace_tests/src/polybench.rs
+++ dace_tests/src/polybench.rs
@@ -4,6 +4,47 @@ use dace::ast::Stmt;
 use dace::loop_node;
 use std::rc::Rc;
 
+pub fn lu(n: usize) -> Rc<Node>{
+    let ubound = n as i32;
+    let ref_a_ij = Node::new_ref("A", vec![n, n], |ijk| {
+        vec![ijk[0] as usize, ijk[1] as usize]
+    });
+    let ref_a_ik = Node::new_ref("A", vec![n, n], |ijk| {
+        vec![ijk[0] as usize, ijk[2] as usize]
+    });
+    let ref_a_kj = Node::new_ref("A", vec![n, n], |ijk| {
+        vec![ijk[2] as usize, ijk[1] as usize]
+    });
+    let ref_a_jj = Node::new_ref("A", vec![n, n], |ijk| {
+        vec![ijk[1] as usize, ijk[1] as usize]
+    });
+
+    let k_loop_ref_j = loop_node!("k", 0 => move |ijk:&[i32]| ijk[1]);
+    Node::extend_loop_body(&k_loop_ref_j, &ref_a_ik);
+    Node::extend_loop_body(&k_loop_ref_j, &ref_a_kj);
+    Node::extend_loop_body(&k_loop_ref_j, &ref_a_ij);
+
+    let j_loop_lower_ref = loop_node!("j", 0 => move |ijk:&[i32]| ijk[0]);
+    Node::extend_loop_body(&j_loop_lower_ref, &k_loop_ref_j);
+    Node::extend_loop_body(&j_loop_lower_ref, &ref_a_jj);
+    Node::extend_loop_body(&j_loop_lower_ref, &ref_a_ij);
+
+    let k_loop_ref_i = loop_node!("k", 0 => move |ijk:&[i32]| ijk[0]);
+    Node::extend_loop_body(&k_loop_ref_i, &ref_a_ik);
+    Node::extend_loop_body(&k_loop_ref_i, &ref_a_kj);
+    Node::extend_loop_body(&k_loop_ref_i, &ref_a_ij);
+
+    let j_loop_upper_ref = loop_node!("j", move |ijk:&[i32]| ijk[0] => ubound);
+    Node::extend_loop_body(&j_loop_upper_ref, &k_loop_ref_i);
+
+    let i_loop_ref = Node::new_single_loop("i", 0, ubound);
+    Node::extend_loop_body(&i_loop_ref, &j_loop_lower_ref);
+    Node::extend_loop_body(&i_loop_ref, &j_loop_upper_ref);
+
+    i_loop_ref
+}
+
+
 fn trmm_trace(M: usize, N: usize) -> Rc<Node> {
     let i_loop_ref = Node::new_single_loop("i", 0, M as i32);
     let j_loop_ref = Node::new_single_loop("j", 0, N as i32);
@@ -278,8 +319,15 @@ mod tests {
     fn test_syr2d() {
         assert_eq!(syr2d(1024, 1024).node_count(), 12);
     }
+
     #[test]
     fn _2mm_test() {
         assert_eq!(_2mm(1024, 1024, 1024, 1024).node_count(), 10);
+    }
+
+    #[test]
+    fn lu_test() {
+        let mm = lu(100);
+        assert_eq!(mm.node_count(), 13);
     }
 }
