5d864314b7918428e61dfb1109f6827274369db5
Farouq Alsalih <farouqalsalih@gmail.com>
2023-06-30 08:13:32 -0400

    Test script

---
+++ tests.sh
@@ -0,0 +1,15 @@
+nohup cargo run --release Olken cholesky falsalih@u.rochester.edu 1 1024 no both &
+nohup cargo run --release Olken cholesky falsalih@u.rochester.edu 1 256 no both &
+nohup cargo run --release Olken cholesky falsalih@u.rochester.edu 1 128 no both &
+nohup cargo run --release Olken cholesky falsalih@u.rochester.edu 1 512 no both &
+nohup cargo run --release Olken lu falsalih@u.rochester.edu 1 100 no both &
+nohup cargo run --release Olken lu falsalih@u.rochester.edu 1 200 no both &
+nohup cargo run --release Olken gramschmidt_trace falsalih@u.rochester.edu 1 1024,1024 no both &
+nohup cargo run --release Olken gramschmidt_trace falsalih@u.rochester.edu 1 512,512 no both &
+nohup cargo run --release Olken gramschmidt_trace falsalih@u.rochester.edu 1 256,256 no both &
+nohup cargo run --release Olken 2mm falsalih@u.rochester.edu 1 1024,1024,1024,1024 no both &
+nohup cargo run --release Olken 2mm falsalih@u.rochester.edu 1 512,512,512,512 no both &
+nohup cargo run --release Olken 2mm falsalih@u.rochester.edu 1 256,256,256,256 no both &
+nohup cargo run --release Olken 2mm falsalih@u.rochester.edu 1 128,128,128,128 no both &
+nohup cargo run --release Olken gemm falsalih@u.rochester.edu 1 128 no both &
+
